3eee45dedbf5777894019727354ca1127ea4911a
rober <rober@DESKTOP-D8M968P>
2025-03-28 17:31:52 +0100

    Segunda Version

---
+++ Git2/src/pruebas/Empleado.java
@@ -0,0 +1,6 @@
+package pruebas;
+
+public class Empleado {
+	private double sueldo;
+
+}
--- Git2/src/pruebas/Persona.java
+++ Git2/src/pruebas/Persona.java
@@ -3,6 +3,8 @@ package pruebas;
 public class Persona {
 	
 	private String nombre;
+	private String apellido;
 	private int edad;
+	
 
 }
