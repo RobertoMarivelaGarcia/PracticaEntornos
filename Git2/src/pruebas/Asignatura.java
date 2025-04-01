d218039846931e74eeae96209bf18e79e58da744
rober <rober@DESKTOP-D8M968P>
2025-04-01 18:42:13 +0200

    Version nueva en rama

---
+++ Git2/src/pruebas/Asignatura.java
@@ -0,0 +1,12 @@
+package pruebas;
+
+public class Asignatura {
+	private String nombre;
+	private String horas;
+
+	
+	Asignatura(String nombre, String horas){
+		this.nombre=nombre;
+		this.horas=horas;
+	}
+}
