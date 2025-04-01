caeccfca2dc72b7ebd68e9bc3fcb1ecc1e456544
rober <rober@DESKTOP-D8M968P>
2025-04-01 19:23:25 +0200

    Version 1.1

--- Git2/src/pruebas/Empleado.java
+++ Git2/src/pruebas/Empleado.java
@@ -2,5 +2,17 @@ package pruebas;
 
 public class Empleado {
 	private double sueldo;
+	
+	Empleado (double sueldo){
+		this.sueldo=sueldo;
+		
+	}
+	public double getSueldo() {
+		return sueldo;
+	}
+
+	public void setSueldo(double sueldo) {
+		this.sueldo = sueldo;
+	}
 
 }
